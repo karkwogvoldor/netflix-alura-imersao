784dbcbdeac17b034cf1ffdf7f33a0c7714c0b39
Jerônimo <karkwogvoldor@gmail.com>
2026-04-08 17:25:43 -0300

    atualizaçao do catalogo

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -1,41 +1,41 @@
 export const categories = [
     {
-        title: "Épicos",
+        title: "Filmes Premiados",
         items: [
             {
-                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+                img: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/aa5b9295-8f9c-44f5-809b-3f2b84badfbf/8a7dd34b09c9c25336a3d850d4c431455e1aaaf0.jpg?host=wbd-images.prod-vod.h264.io&partner=beamcom", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=i6avfCqKcQo"
             },
             {
-                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToui8ZR9qXo9iZXJyL_o9JHdkrFAwcGsaHNw&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=0mCZqtqPa1k"
             },
             {
                 img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
             },
             {
-                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+                img: "https://i.ytimg.com/vi/3OZ2QZLulTU/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=1OeQtW9KCEU"
             },
         ]
     },
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwwlfzrNA316N8EVOlnTT0DFABCqUXl9Gtmg&s", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "youtube.com/watch?v=9RNHKKYJvg8&time_continue=1&source_ve_path=MjM4NTE&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dtrailer%2Bdark%2B3%2Btemporada%26rlz%3D1C1GCEA_enBR931BR931%26oq%3Dtrailer%2Bdark%2B3%2Btemporada%26gs_lcrp%3DEgZjaHJvbW" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNlO-lShFGpWTgM1K5MFEfOZmNRiZHDxdquw&s", top10: true, youtube: "https://www.youtube.com/watch?v=_-Nv0JOcGxg" },
+            { img: "https://cinema10.com.br/upload/series/series_1805_gmaxresdefault-easy-resize.com.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=909qosDbalU" },
+            { img: "https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=RMmGQNNl164" },
         ]
     },
     {
-        title: "Para maratonar",
+        title: "Animes para maratonar",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://www.pngarts.com/files/11/Naruto-Shippuden-Logo-PNG-Pic.png", top10: true, youtube: "https://www.youtube.com/watch?v=06EjEq_7RJY" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlNj5FPbtbr_A6TmctyBYnekyPbEGwiD6bgQ&s", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=4cX-CjdW_gY" },
+            { img: "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/01184a0e8912da9e3115536752cfa1db.png", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=MCb13lbVGE0" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCwK_rj0CxQ1f4L7my0Uwqd4CqGi3Szec-SQ&s", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=-G9BqkgZXRA" },
         ]
     }
 ];
